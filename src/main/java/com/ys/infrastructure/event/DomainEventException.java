08430f00acb5a32febd7b9abb0160881268455c7
youngseok <dblys152@gmail.com>
2024-02-02 16:37:44 +0900

    도메인 이벤트 추가

---
+++ src/main/java/com/ys/infrastructure/event/DomainEventException.java
@@ -0,0 +1,13 @@
+package com.ys.infrastructure.event;
+
+import lombok.Getter;
+
+public class DomainEventException extends RuntimeException {
+    @Getter
+    private final String eventType;
+
+    public DomainEventException(String eventType, Exception e) {
+        super(e);
+        this.eventType = eventType;
+    }
+}
